b0475d37b0e319db2c5e6effc94dcc74c4cb268d
Pramila Shanmugam <pramila.sl@gmail.com>
2024-09-09 10:46:18 +0000

    intial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
 import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
+import Content from "./components/Content";
+import NavBarChild from './components/NavBarChild';
+import NavBarForm from './components/NavBarForm';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <Content />
+      <NavBarChild />
+      <NavBarForm />
+      <NavBarSimple />
       <Sidebar />
     </div>
   );
